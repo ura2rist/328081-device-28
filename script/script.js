bdda4453f838ab936449d847036572672eb3764f
Горячев Юрий Александрович <Goryachev_UA@pkb1.local>
2020-07-08 19:37:37 +0300

    Правки и скрипт

--- script/script.js
+++ script/script.js
@@ -1,23 +1,27 @@
-function viewForm(e){
-	e.preventDefault();
-	let modal = document.querySelector('#modal-form');
-	modal.classList.toggle('view-form');
-}
+document.getElementById('view-form').addEventListener('click', function(event){
+	formToggle(event);
+});
 
-function closeForm(e){
-	e.preventDefault();
-	let modal = document.querySelector('#modal-form');
-	modal.classList.toggle('view-form');
-}
+document.getElementById('close-form').addEventListener('click', function(event){
+	formToggle(event);
+});
 
-document.getElementById('map').onclick = function(event){
-	event.preventDefault();
+document.getElementById('map').addEventListener('click', function(event){
+	mapToggle(event);
+});
+
+document.getElementById('map-close').addEventListener('click', function(event){
+	mapToggle(event);
+});
+
+function mapToggle(e){
+	e.preventDefault();
 	let map = document.querySelector('.map');
 	map.classList.toggle('map-active');
 }
 
-document.querySelector('.map').onclick = function(event){
-	event.preventDefault();
-	let map = document.querySelector('.map');
-	map.classList.toggle('map-active');
+function formToggle(e){
+	e.preventDefault();
+	let modal = document.querySelector('#modal-form');
+	modal.classList.toggle('view-form');
 }
