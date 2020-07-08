8c88aea8d923963ff731f0ad68f553f1744bec1e
Горячев Юрий Александрович <Goryachev_UA@pkb1.local>
2020-07-08 16:54:10 +0300

    форма и карта

--- script/script.js
+++ script/script.js
@@ -9,3 +9,15 @@ function closeForm(e){
 	let modal = document.querySelector('#modal-form');
 	modal.classList.toggle('view-form');
 }
+
+document.getElementById('map').onclick = function(event){
+	event.preventDefault();
+	let map = document.querySelector('.map');
+	map.classList.toggle('map-active');
+}
+
+document.querySelector('.map').onclick = function(event){
+	event.preventDefault();
+	let map = document.querySelector('.map');
+	map.classList.toggle('map-active');
+}
